dc657d5ae6e9a8f1531d1f040fcbb1b7f8a605dd
calmis <calmis87@gmail.com>
2021-02-28 14:58:14 +0200

    add labels

--- sapior.py
+++ sapior.py
@@ -5,6 +5,8 @@ window=Tk()
 
 files=[]
 btn=[]
+files2=[]
+label=[]
 
 window.wm_title("Sapior")
 window.resizable(width=0, height=0)
@@ -14,46 +16,48 @@ def hide_button(i):
     print(i)
     print(btn[int(i)])
     btn[int(i)].grid_forget()
+    #label[int(i)].config(fg="green")#change state of label under buttons
 
-#top of window
-l1=Label(window,text="time")
-l1.grid(row=0,column=0,columnspan=8)
-l2=Label(window,text="nr")
-l2.grid(row=0,column=6,columnspan=7)
+def create_top():
+    #top of window
+    l1=Label(window,text="time")
+    l1.grid(row=0,column=0,columnspan=8)
+    l2=Label(window,text="nr")
+    l2.grid(row=0,column=6,columnspan=7)
 
-# for i in range(15):
-#     for j in range(12):
-#         b=Button(window,text="",width=2,command=open)
-#         b.grid(row=j+1,column=i)
+def arrange_buttons():
+    #create 180 buttons with text
+    for i in range(180):
+        files.append(str(i))
 
-#create 180 buttons with text
-for i in range(180):
-    files.append(str(i))
+    for i in range(len(files)):
+        btn.append(Button(window,text=files[i],width=2,command=lambda c=i:hide_button(btn[c].cget("text"))))#fg="linen",
 
-for i in range(len(files)):
-    btn.append(Button(window,text=files[i],width=2,command=lambda c=i:hide_button(btn[c].cget("text"))))#fg="linen",
+    #arange all button in the window
+    for j in range(0,12):
+        for i in range(15*j,15*(j+1)):
+            print(j, ' ', i)
+            btn[i].grid(row=j,column=i-15*j)
 
-# for j in range(1):
-#     for i in range(int(len(btn)/3)):
-#         print(j, ' ', i)
-#         btn[i].grid(row=j,column=i)
-#
-# for j in range(1,2):
-#     for i in range(int(len(btn)/3), len(btn)-15):
-#         print(j, ' ', i)
-#         btn[i].grid(row=j,column=i-15)
-#
-# for j in range(2,3):
-#     for i in range(int(len(btn)/3)*2, len(btn)):
-#         print(j, ' ', i)
-#         btn[i].grid(row=j,column=i-30)
+def arrange_labels():
+    #create 180 labels with text
+    for i in range(180):
+        files2.append(str(i))
 
-#arange all button in the window
-for j in range(0,12):
-    for i in range(15*j,15*(j+1)):
-        print(j, ' ', i)
-        btn[i].grid(row=j,column=i-15*j)
+    for i in range(len(files2)):
+        label.append(Label(window,text=files2[i],fg="red",width=2))
 
+    #arange all button in the window
+    for j in range(0,12):
+        for i in range(15*j,15*(j+1)):
+            print(j, ' ', i)
+            label[i].grid(row=j,column=i-15*j)
 
+def init_window():
+    create_top()
+    arrange_labels()
+    arrange_buttons()
+
+init_window()
 
 window.mainloop()
